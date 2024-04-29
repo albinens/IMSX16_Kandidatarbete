9b88927230f1abba415be5dcfbdee9d69b521990
Cronvall <47293083+Cronvall@users.noreply.github.com>
2024-04-29 10:46:53 +0200

    about page

---
+++ frontend/app/src/pages/about.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const About = () => {
+  return (
+    <div style={{textAlign: "center"}}>
+      <h1 style={{marginTop: "12vh"}}>About</h1>
+      <p>This solution has been developed during a Bachelors thesis work at Chlamers University of Technology</p>
+      <p>The software has been developed by and all rights to the software belong to us under copyright: <br/> Felix Bjerhem Aronsson, Oscar Cronvall, Albin Enström, Moa Johannesson, Tim Karlsson, Lukas Åkefeldt</p>
+      <p>Supervisor: Erik Hulthén</p>
+      <h2> Give feedback on the project here:</h2>
+      <a href="https://forms.office.com/e/F5j0pRjXtj">Feedback Form</a>
+    </div>
+  )
+}
+
+export default About;
